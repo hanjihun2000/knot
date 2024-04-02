c1fc3cdb69b830712701133d2eb5923c0fd6f61f
hksquinson <hksquinson@gmail.com>
2024-04-02 20:12:31 +0800

    mongo setup + change port 5000

--- server/index.js
+++ server/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-const PORT = 5000;
+const PORT = 8000;
 
 const app = express();
+
+
+const uri = "mongodb+srv://admin1:1234567890@knot-cluster.ggtkwpg.mongodb.net/?retryWrites=true&w=majority&appName=knot-cluster";
+const client = new MongoClient(uri, { 
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	}
+});
+
+client.connect();
+
+async function listDatabases(client){
+	databasesList = await client.db().admin().listDatabases();
+	console.log("Databases:");
+	databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
+listDatabases(client);
 
 app.get("/api", (req, res) => {
     res.json({ message: "Hello from server!" });
