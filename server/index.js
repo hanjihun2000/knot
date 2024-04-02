e6d99099992ec7c03722e6388c9ddc61f5080966
hanjihun2000 <1155128719@link.cuhk.edu.hk>
2024-04-02 20:32:37 +0800

    mongoose

--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const express = require("express");
-
+const app = express();
 const PORT = 5000;
 
-const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin1:1234567890@knot-cluster.ggtkwpg.mongodb.net/?retryWrites=true&w=majority&appName=knot-cluster";
+async function connectToMongoDB() {
+	try {
+		const client = new MongoClient(uri, { 
+			serverApi: {
+				version: ServerApiVersion.v1,
+				strict: true,
+				deprecationErrors: true,
+			}
+		});
+		await client.connect();
+		console.log("Connected to MongoDB");
+	}
+	catch (err) {
+		console.error(err);
+	}
+};
+connectToMongoDB();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+	res.json({ message: "API Working" });
 });
 
 app.listen(PORT, () => {
