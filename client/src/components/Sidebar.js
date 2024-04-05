e3648b934cc6050047982b2f2065ed5ecc7fc616
sennett cheng <sennett@regentsoft.com.hk>
2024-04-06 00:37:18 +0800

    completed the list of item without interactive bell

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import '../component_css/Sidebar.css';
+import {sideBarItem} from './sideBarItem';
+import { useHistory } from 'react-router-dom';
 
 const Sidebar = () => {
+  const history = useHistory();
+
   return (
     <nav className="sidebar">
-      <button className="menu-item-create">create</button>
-      <button className="menu-item">home page</button>
-      <button className="menu-item">search</button>
-      <button className="menu-item">settings</button>
-      <button className="logout">Logout</button>
+      <button className="menu-item-button-create">create</button>
+      <ul className="sidebar-list">
+      {sideBarItem.map((item, index) => {
+        return (
+          <li 
+          key={index} 
+          className="row"
+          id = {window.location.pathname === item.link ? "active" : ""}
+          onClick={() => history.push(item.link)}>
+            <div id="menu-item-icon">{item.icon}</div>
+            <div id="menu-item-text">{item.title}</div>
+          </li>
+        );
+      })}
+      </ul>
+
+      <div className="profile-section">
+        <img src="https://via.placeholder.com/150" alt="Profile" className="profile-pic" />
+        <div className="profile-name">Profile Name</div> {/* replace with the actual profile name */}
+        <hr className="separator" />
+        <button className="logout">Logout</button>
+      </div>
     </nav>
   );
 };
