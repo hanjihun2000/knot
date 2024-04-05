76ba4c79751f03d34d4c6aa0a9912f756c488964
TPTK <DanielSHuq@proton.me>
2024-04-06 01:41:39 +0800

    d
    s

--- client/src/components/LogInForm.js
+++ client/src/components/LogInForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../component_css/LogInForm.css'; // Make sure to create a corresponding CSS file
+import './component_css/LogInForm.css'; // Make sure to create a corresponding CSS file
 import logo from '../unnamed.png';
 import toggleVisi from '../OIP.jpg';
 import {Link} from 'react-router-dom';
