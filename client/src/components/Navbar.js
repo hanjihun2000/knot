415a52141cba35bf45888f7a49480c7a9bdcaa2a
hanjihun2000 <1155128719@link.cuhk.edu.hk>
2024-04-10 03:30:10 +0800

    update search

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
-import './component_css/Navbar.css';
-import { List} from "@phosphor-icons/react";
-import { useSideBarContext } from './SidebarComp/SideBarContext';
+import "./component_css/Navbar.css";
+import { List } from "@phosphor-icons/react";
+import { useSideBarContext } from "./SidebarComp/SideBarContext";
 const Navbar = () => {
   const { isOpen, setIsOpen } = useSideBarContext(); // Use context to get state and updater
 
@@ -8,15 +8,12 @@ const Navbar = () => {
     setIsOpen(!isOpen); // Toggle the state based on its current value
   };
 
-  
-
   return (
     <div className="navbar">
-        <button onClick={handleButtonClick} className="List-icon-button">
-          <List className="List-icon" />
-        </button>
-        <h1 className="navbar-title">Knot</h1>
-      <input type="text" placeholder="Search..." className="search-bar"/>
+      <button onClick={handleButtonClick} className="List-icon-button">
+        <List className="List-icon" />
+      </button>
+      <h1 className="navbar-title">Knot</h1>
     </div>
   );
 };
--- client/src/components/UserSettings/SearchPage.js
+++ client/src/components/UserSettings/SearchPage.js
@@ -1,32 +1,33 @@
-import { NavLink } from 'react-router-dom/cjs/react-router-dom';
-import '../component_css/SearchPage.css';
-import React, { useState, useEffect } from 'react';
-
+import { NavLink } from "react-router-dom/cjs/react-router-dom";
+import "../component_css/SearchPage.css";
+import React, { useState, useEffect } from "react";
 
 const SearchPage = ({ users, onSearch }) => {
-    return (
-        <div className="search-page">
-          <div className="results-container">
-            {users && users.length > 0 ? (
-              users.map((user) => (
-                <div key={user.id} className="user-card">
-                  <NavLink to={`/profile/${user.username}`} className="profile-link">
-                    <img
-                      src={user.profilePicture}
-                      alt={user.username}
-                      className="profile-picture"
-                    />
-                    <span className="username">{user.username}</span>
-                  </NavLink>
-                </div>
-              ))
-            ) : (
-              <p>No results found.</p>
-            )}
-          </div>
-        </div>
-      );
-    };
-    
+  return (
+    <div className="search-page">
+      <div className="results-container">
+        {users && users.length > 0 ? (
+          users.map((user) => (
+            <div key={user.username} className="user-card">
+              <NavLink
+                to={`/profile/${user.username}`}
+                className="profile-link"
+              >
+                <img
+                  src={user.profilePicture}
+                  alt={user.username}
+                  className="profile-picture"
+                />
+                <span className="username">{user.username}</span>
+              </NavLink>
+            </div>
+          ))
+        ) : (
+          <p>No results found.</p>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default SearchPage;
